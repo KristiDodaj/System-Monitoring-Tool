d1fcc264b2102a0e0c8d2e2e63058db33c653c62
KristiDodaj <kristidodaj001@gmail.com>
2023-01-26 22:16:17 -0500

    added the get users functionality

--- mySystemStats.C
+++ mySystemStats.C
@@ -1,10 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/utsname.h>
+#include <utmpx.h>
 
-// This function will print out the System Information using the <sys/utsname.h> C library
 void getSystemInfo()
 {
+    // This function will print out the System Information using the <sys/utsname.h> C library
+    // Examle Output:
+    // getSystemInfo() returns
+    // System Name = Darwin
+    // Machine Name = Kristis-MacBook-Air.local
+    // Version = Darwin Kernel Version 22.1.0: Sun Oct  9 20:14:30 PDT 2022; root:xnu-8792.41.9~2/RELEASE_ARM64_T8103
+    // Release = 22.1.0
+    // Architecture = arm64
 
     // create and populate the struct
     struct utsname info;
@@ -25,8 +33,25 @@ void getSystemInfo()
     }
 }
 
+void getUsers()
+{
+    struct utmpx *users;
+    setutxent(); // rewinds pointer to beginning of utmpx file
+
+    // ASK IF CHECKING THAT THIS IS A USER_PROCESS
+
+    while ((users = getutxent()) != NULL)
+    {
+        if (users->ut_type == USER_PROCESS)
+        {
+            printf("%s      %s (%s) \n", users->ut_user, users->ut_line, users->ut_host);
+        }
+    }
+    endutxent(); // close the utmpx file
+}
+
 int main()
 {
-    getSystemInfo();
+    getUsers();
     return 0;
 }
