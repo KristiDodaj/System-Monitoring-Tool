b6389acb00046a4a845565547a12b9b54861af0e
KristiDodaj <kristidodaj001@gmail.com>
2023-02-02 18:48:39 -0500

    test

--- mySystemStats.C
+++ mySystemStats.C
@@ -486,7 +486,6 @@ int main(int argc, char *argv[])
     bool sequential = false;
     int samples = 10;
     int tdelay = 1;
-    printf("Samples: %d, Tdelay: %d", samples, tdelay);
 
     parseArguments(argc, argv, &system, &user, &sequential, &samples, &tdelay);
 
