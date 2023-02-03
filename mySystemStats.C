9f50e3c1b902dd7730957157ad695ce1f096a51f
KristiDodaj <kristidodaj001@gmail.com>
2023-02-03 16:57:44 -0500

    test

--- mySystemStats.C
+++ mySystemStats.C
@@ -524,9 +524,15 @@ bool validateArguments(int argc, char *argv[])
     // iterate argv to check for correctness
     for (int i = 1; i < argc; i++)
     {
+
+        char *firstPostinalArg;
+        char *secondPositionalArg;
+        strtol(argv[1], &firstPostinalArg, 10);
+        strtol(argv[2], &secondPositionalArg, 10);
+
         // check if all the flags are correctly formated
 
-        if (strcmp(argv[i], "--sequential") != 0 && strcmp(argv[i], "--system") != 0 && strcmp(argv[i], "--user") != 0 && sscanf(argv[i], "--samples=%d", &dummyValue) != 1 && sscanf(argv[i], "--tdelay=%d", &dummyValue) != 1 && sscanf(argv[1], "%d", &dummyValue) != 1 && sscanf(argv[2], "%d", &dummyValue) != 1)
+        if (strcmp(argv[i], "--sequential") != 0 && strcmp(argv[i], "--system") != 0 && strcmp(argv[i], "--user") != 0 && sscanf(argv[i], "--samples=%d", &dummyValue) != 1 && sscanf(argv[i], "--tdelay=%d", &dummyValue) != 1 && *firstPostinalArg != '\0' && *secondPositionalArg != '\0')
         {
             printf("ONE OF YOUR ARGUMENTS IS MISTYPED OR IN THE WRONG ORDER. TRY AGAIN!");
             return false;
