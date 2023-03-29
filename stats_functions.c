db74afd0f489488591a7de5270fc9d94601b1982
KristiDodaj <kristidodaj001@gmail.com>
2023-03-29 17:51:44 -0400

    cpu

--- stats_functions.c
+++ stats_functions.c
@@ -259,10 +259,14 @@ void allInfoUpdate(int samples, int tdelay)
         getUsers();
         printf("---------------------------------------\n");
         getCpuNumber();
-        if ((i != 0) && (i != (samples - 1)))
+        if (i > 0)
         {
-            // print usage
-            printf(" total cpu use = %.10f %%\n", usage);
+            printf("here");
+            if (i != samples - 1)
+            {
+                // print usage
+                printf(" total cpu use = %.10f %%\n", usage);
+            }
         }
         usage = getCpuUsage(tdelay); // get current measurement for cpu usage
         if (i == samples - 1)
