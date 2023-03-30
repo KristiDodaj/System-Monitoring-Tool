c9a025598620120264caa531da9a68977876b742
KristiDodaj <kristidodaj001@gmail.com>
2023-03-30 17:19:54 -0400

    cpu

--- stats_functions.c
+++ stats_functions.c
@@ -321,34 +321,37 @@ void allInfoUpdate(int samples, int tdelay)
     // print all information
     for (int i = 0; i < samples; i++)
     {
-        // print output
-        printf("\033[%d;0H", (memoryLineNumber)); // move cursor to memory
-        getMemoryUsage();
-        printf("\033[%d;0H", (usersLineNumber)); // move cursor to users
-        printf("---------------------------------------\n");
-        printf("### Sessions/users ###\n");
-        printf("\033[J"); // clears everything below the current line
-        getUsers();
-        printf("---------------------------------------\n");
-        getCpuNumber();
-
-        if (i > 0)
+        if (i != samples - 1)
         {
+            // print output
+            printf("\033[%d;0H", (memoryLineNumber)); // move cursor to memory
+            getMemoryUsage();
+            printf("\033[%d;0H", (usersLineNumber)); // move cursor to users
+            printf("---------------------------------------\n");
+            printf("### Sessions/users ###\n");
+            printf("\033[J"); // clears everything below the current line
+            getUsers();
+            printf("---------------------------------------\n");
+            getCpuNumber();
 
-            // print usage
-            printf(" total cpu use = %.10f %%\n", usage);
+            if (i > 0)
+            {
+
+                // print usage
+                printf(" total cpu use = %.10f %%\n", usage);
+            }
+
+            usage = getCpuUsage(tdelay); // get current measurement for cpu usage
+
+            // if (i == samples - 1)
+            //{
+            // print usage for last iteration
+            //     printf(" total cpu use = %.10f %%\n", usage);
+            // }
+
+            // update line numbers
+            memoryLineNumber = memoryLineNumber + 1;
         }
-
-        usage = getCpuUsage(tdelay); // get current measurement for cpu usage
-
-        // if (i == samples - 1)
-        //{
-        // print usage for last iteration
-        //     printf(" total cpu use = %.10f %%\n", usage);
-        // }
-
-        // update line numbers
-        memoryLineNumber = memoryLineNumber + 1;
 
         if (i == samples - 1)
         {
