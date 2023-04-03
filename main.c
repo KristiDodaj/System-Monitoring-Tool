8e88f519a6ee8f6cce194f84f70b5233a34f2838
KristiDodaj <kristidodaj001@gmail.com>
2023-04-03 16:14:44 -0400

    signals

--- main.c
+++ main.c
@@ -371,7 +371,7 @@ int main(int argc, char *argv[])
     // navigate(argc, argv);
 
     getCpuUsageGraphic(5, 3, 11);
-    getMemoryUsageGraphic(9.85, 9.76);
+    getMemoryUsageGraphic(9.85000, 9.76000);
 
     return 0;
 }
