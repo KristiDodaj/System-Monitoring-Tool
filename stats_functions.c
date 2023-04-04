2ecf84abb91e365a3ecc72250b389c31c2e9d132
KristiDodaj <kristidodaj001@gmail.com>
2023-04-03 20:05:34 -0400

    signals

--- stats_functions.c
+++ stats_functions.c
@@ -1411,7 +1411,7 @@ void systemUpdateGraphic(int samples, int tdelay)
             float dummy3;
             sscanf(buf, "%f GB / %f GB  --  %f GB / %f GB\n", &dummy, &dummy2, &usage, &dummy3);
 
-            printf("%s   ", buff);
+            printf("%s   ", buf);
 
             // add to array
             memory_usage[i] = usage;
