97e17c450ab94b8a25d2a8781176b57b32723274
KristiDodaj <kristidodaj001@gmail.com>
2023-04-03 22:49:24 -0400

    signals

--- main.c
+++ main.c
@@ -370,7 +370,7 @@ int main(int argc, char *argv[])
     // call the navigate function which will redirect to the right output depeneding on the arguments
     // navigate(argc, argv);
 
-    systemSequentialGraphic(5, 1);
+    systemSequentialGraphic(2, 2);
 
     return 0;
 }
